6960741e7a2e52b9fd1c4a805ed861670128709a
mdbudd <matt@mattbudd.co.uk>
2018-04-14 00:34:18 +0100

    further work on exploring component and using fitBounds

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -8,13 +8,13 @@ class MapComponent extends Component {
     selectedPlace: {},
   };
 
-  onMarkerClick = (props, marker, e) => {
+  onMarkerClick = (props, marker, e) => (
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true
-    });
-  }
+    })
+  )
 
   onMapClicked = (props) => {
     if (this.state.showingInfoWindow) {
@@ -25,28 +25,49 @@ class MapComponent extends Component {
     }
   };
 
+  componentDidUpdate() {
+    const bounds = new window.google.maps.LatLngBounds()
+    this.state.results.map((result) => {
+      bounds.extend(new window.google.maps.LatLng(
+        result.latitude,
+        result.longitude
+      ));
+    });
+
+    this.refs.resultMap.fitBounds(bounds)
+  }
+
 
   render() {
     const style = {
       width: '80%',
-      position:'relative',
-      margin:'0 auto',
+      position: 'relative',
+      margin: '0 auto',
       height: '100vh'
     }
-   
+
     if (!this.props.loaded) {
       return <div>Loading...</div>
     }
+    console.log(this.props)
     return (
       <div style={style}>
-        <Map google={this.props.google} zoom={14}>
-          <Marker
-            onClick={this.onMarkerClick}
-            name={'Current location'}
-          />
+        <Map google={this.props.google}
+          onClick={this.onMapClicked}
+          ref="resultMap">
+          <Marker onClick={this.onMarkerClick}
+            name={'My location'}
+            position={{ lat: 50.715536, lng: -1.8734264 }} />
+          <Marker onClick={this.onMarkerClick}
+            title={'The marker`s title will appear as a tooltip.'}
+            name={'SOMA'}
+            position={{ lat: 37.778519, lng: -122.405640 }} />
+          <Marker onClick={this.onMarkerClick}
+            name={'Dolores park'}
+            position={{ lat: 37.759703, lng: -122.428093 }} />
           <InfoWindow
-            onClose={this.onInfoWindowClose}
-          >
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
             <div>
               <h1>{this.state.selectedPlace.name}</h1>
             </div>
